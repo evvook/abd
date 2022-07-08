f772e97a2c7dcd11ec20c1adf64e6f6c0cd022d1
evvook <sthalunar@gmail.com>
2022-07-08 20:31:56 +0900

    Delete Sample Codes

--- src/main/java/abd/service/CharacterService.java
+++
@@ -1,8 +0,0 @@
-package abd.service;
-
-import java.util.List;
-import java.util.Map;
-
-public interface CharacterService {
-	public List<Map<String,String>> selectCharacter(Map<String,String> paramMap) throws Exception;
-}
